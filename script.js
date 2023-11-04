f1ef722e35e408f9c8926d1e4d9eb7a89aaee17f
Prashanth14 <kadireprashanthreddy1@gmail.com>
2023-11-04 13:09:08 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+
+
+//functions
+function addItem(e){
+	e.preventDefault();
+
+	const newItem = itemInput.value;
+	//Validate Input
+   if(newItem === ''){
+        alert('please add an item');
+        return;
+   }
+    //create list item
+	const li = document.createElement('li');
+	li.appendChild(document.createTextNode(newItem));
+
+	//create a button
+	const button = createButton('remove-item btn-link text-red');
+
+	li.appendChild(button);
+	itemList.appendChild(li);
+
+	itemInput.value = '';
+}
+
+//create button function
+function createButton(classes){
+	const button = document.createElement('button');
+	button.className = classes;
+	//create icon
+	const icon = createIcon('fa-solid fa-xmark');
+	button.appendChild(icon);
+	return button;
+}
+
+//Create Icon function
+function createIcon(classes){
+	const icon = document.createElement('i');
+	icon.className = classes;
+	return icon;
+}
+
+
+//Event Listeners
+itemForm.addEventListener('submit', addItem);
